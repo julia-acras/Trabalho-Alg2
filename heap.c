c9b8c7a4931d0c2823b1131f3505c9fd31261bb4
JULIA SILVEIRA ACRAS <jsa25@inf.ufpr.br>
2025-10-28 16:19:43 -0300

    arrumando heap

--- heap.c
+++ heap.c
@@ -18,7 +18,7 @@ struct paciente *InicHeap(int tam)
 {
     struct paciente *fila;
 
-    fila = malloc(tam * sizeof(struct paciente))
+    fila = malloc(tam * sizeof(struct paciente));
 
     if (fila == NULL)
         return(NULL);
@@ -27,12 +27,13 @@ struct paciente *InicHeap(int tam)
 }
 
 
-void InsereHeap(int tam, int vetor[])
+void InsereHeap(int tam, struct paciente vetor[])
 {
-    int i, j, aux;
+    int i, j;
+    struct paciente aux;
 
     j = tam+1;
-    while(j > 1 && vetor[i/2] < vetor[i])
+    while(j > 1 && vetor[i/2].prioridade < vetor[i].prioridade)
     {
         //troca
         aux = vetor[i/2];
@@ -42,45 +43,51 @@ void InsereHeap(int tam, int vetor[])
     }
 }
 
+void Heapfy (int N, struct paciente V[]) 
+{
+    int j;
+    for (j = 1; j < N; j++) 
+        InsereHeap(j,V);
+}
+
+int checaheap(struct paciente v[], int N){
+    int i=N;
+    for ( i=N;i>1;i--){
+        if (v[i].prioridade>v[i/2].prioridade)
+            return 0;}
+    return 1;
+}
+
 void RemoveHeap (int N, struct paciente V[], struct paciente p)
 {
     int i; 
 
     while ((i <= N) && ((strcmp(V[i].nome, p.nome) != 0) && (V[i].prioridade != p.prioridade))) i++;
-
-    for (i, i <= N, i++) V[i] = V[i + 1];
-
+    {
+        for (i; i <= N; i++) 
+            V[i] = V[i + 1];
+    }   
     (N)--;
 
-    if (ChecaHeap(N, V) == 0) Heapfy(N, V);
+    if (checaheap(V, N) == 0) Heapfy(N, V);
 }
 
-void Heapfy (int N, int V[]) 
-{
-    int j;
-    for (j = 1; j < N; j++) InsereHeap(j,V);
-}
 
-int checaheap( int v[]){
-    int i=N;
-    for ( i=N;i>1;i--){
-        if (v[i]>v[i/2])
-            return 0;}
-    return 1;
-}
 
-void imprimeHeap(Paciente v[], int N) {
+void imprimeHeap(struct paciente v[], int N) {
     for (int i = 1; i <= N; i++) {
         printf("%s (prioridade %d)\n", v[i].nome, v[i].prioridade);
     }
 }
 
-void SacodeHeap(int tam, int v[]) {
-    int aux, i = 2;
+void SacodeHeap(int tam, struct paciente v[]) {
+    int i = 2;
+    struct paciente aux;
+
     while (i <= tam) {
-        if (i < tam && v[i] < v[i + 1])
+        if (i < tam && v[i].prioridade < v[i + 1].prioridade)
             i++;
-        if (v[i/2] >= v[i])
+        if (v[i/2].prioridade >= v[i].prioridade)
             break;
         aux = v[i/2];
         v[i/2] = v[i];
@@ -90,8 +97,10 @@ void SacodeHeap(int tam, int v[]) {
 } 
 
 void HeapSort(struct paciente vetor[], int N) {
-    int i, aux;
-    Heapfy(vetor);
+    int i;
+    struct paciente aux;
+
+    Heapfy(N, vetor);
     for (i = N; i > 1; i--) {
         aux = vetor[1];
         vetor[1] = vetor[i];
@@ -101,10 +110,10 @@ void HeapSort(struct paciente vetor[], int N) {
 }
 
 
-void AlteraHeap(struct paciente vetor[], int posicao, int prioridade) {
+void AlteraHeap(struct paciente vetor[], int posicao, int prioridade, int tam) {
     vetor[posicao].prioridade = prioridade;
-    if (!ChecaHeap(vetor)) 
-        Heapfy(vetor);
+    if (!checaheap(tam, vetor)) 
+        Heapfy(tam, vetor);
 }
 
 
