5cee136b752f13852b853f6f8f7707cfa955369c
Julia Acras <juliaacras@ufpr.br>
2025-10-29 19:45:12 -0300

    começo do menu

--- menu.c
+++ menu.c
@@ -16,3 +16,36 @@ para a Enfermeira Chefe usar seu sistema.*/
 com 1024 elementos e mostre a comparação do HeapSort com o QuickSort e o SelectSort. 
 Qual foi melhor na prática? Para cada algoritmo mostre o número de comparações 
 e trocas de elemento de lugar.*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main()
+{
+    int caminho;
+    printf("MENU PRONTO SOCORRO HEAP OR QUICK:\n");
+    printf("ALERTA: Somente aperte botões sugeridos pelo menu.\n");
+	printf("Se você apertar alguma tecla errada, REINICIE O PROGRAMA!\n");
+	printf("\n");
+
+    printf("Escolha qual caminho seguir:\n");
+    printf("1 - Menu Pronto Socorro ""Heap or Quick\n");
+    printf("2 - Ordenação de vetor com números aleatórios\n");
+    printf(">>");
+    scanf("%d", &caminho);
+    printf("\n");
+    if(caminho == 1)
+    {
+        printf("Você entrou no menu do pronto socorro Heap or Quick.\n");
+    }
+    else if (caminho == 2)
+    {  
+        printf("Você entrou na área de ordenação de vetor.\n");
+    }
+
+
+
+    return (0);
+}
+
